452386d840dca459a9ecbaa32f913a73711004e8
Simon Misencik <simon.misencik@gmail.com>
2022-11-18 13:25:16 +0100

    Remove unnecessary changes
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- core/src/test/java/com/netflix/conductor/service/MetadataServiceTest.java
+++ core/src/test/java/com/netflix/conductor/service/MetadataServiceTest.java
@@ -19,7 +19,6 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolationException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
