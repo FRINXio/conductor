7f5196e5fbae62ee82621d283c18ab514dadfc10
Simon Misencik <simon.misencik@gmail.com>
2022-11-24 14:02:47 +0100

    Fix connection issue
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- postgres-external-storage/src/main/java/com/netflix/conductor/postgres/storage/PostgresPayloadStorage.java
+++ postgres-external-storage/src/main/java/com/netflix/conductor/postgres/storage/PostgresPayloadStorage.java
@@ -17,6 +17,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 
 import javax.sql.DataSource;
